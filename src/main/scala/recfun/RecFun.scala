c53c8562b9eff2a774ae161122b15df8b3f4ad86
Alexey Kudryashov <akd@krononsoft.com>
2023-09-07 17:47:22 +0300

    Cleanup and refactoring

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -14,40 +14,30 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-  def pascal(col: Int, row: Int): Int = {
-    if (col == 0 || row == col) {
+  def pascal(col: Int, row: Int): Int =
+    if (col == 0 || row == col)
       1
-    } else {
+    else
       pascal(col - 1, row - 1) + pascal(col, row - 1)
-    }
-  }
 
   /**
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
     @tailrec
-    def wereAllBracketsClosed(chars: List[Char], opensCount: Int): Boolean = {
-      if (chars.isEmpty) {
-        opensCount == 0
-      } else {
-        val headSymbol: Char = chars.head
-        val newOpensCount =
-          if (headSymbol == '(') {
-            opensCount + 1
-          } else if (headSymbol == ')') {
-            opensCount - 1
-          } else {
-            opensCount
-          }
+    def wereAllBracketsClosed(chars: List[Char], opensCount: Int): Boolean =
+      if (chars.isEmpty) return opensCount == 0
+      if (opensCount < 0) return false
 
-        if (newOpensCount >= 0) {
-          wereAllBracketsClosed(chars.tail, newOpensCount)
-        } else {
-          false
-        }
-      }
-    }
+      var newOpensCount: Int = opensCount
+
+      if (chars.head == '(')
+        newOpensCount += 1
+
+      if (chars.head == ')')
+        newOpensCount -= 1
+
+      wereAllBracketsClosed(chars.tail, newOpensCount)
 
     wereAllBracketsClosed(chars, 0)
   }
@@ -55,18 +45,10 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-    if (coins.isEmpty) {
-      0
-    } else {
-      if (money == 0) {
-        1
-      } else if (money < 0) {
-        0
-      }
-      else {
-        countChange(money - coins.head, coins) + countChange(money, coins.tail)
-      }
-    }
-  }
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (coins.isEmpty || money < 0) return 0
+    if (money == 0) return  1
 
+    countChange(money - coins.head, coins) + countChange(money, coins.tail)
+
+@main def hello() = println("Hello, World")
