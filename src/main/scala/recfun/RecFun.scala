088a614e092b92627032411d50d0150adbabba25
Alexey Kudryashov <akd@krononsoft.com>
2023-09-11 13:20:17 +0300

    Refactoring, part 2

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -15,7 +15,7 @@ object RecFun extends RecFunInterface:
    * Exercise 1
    */
   def pascal(col: Int, row: Int): Int =
-    if (col == 0 || row == col)
+    if col == 0 || row == col then
       1
     else
       pascal(col - 1, row - 1) + pascal(col, row - 1)
@@ -23,32 +23,31 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = {
+  def balance(chars: List[Char]): Boolean =
     @tailrec
     def wereAllBracketsClosed(chars: List[Char], opensCount: Int): Boolean =
-      if (chars.isEmpty) return opensCount == 0
-      if (opensCount < 0) return false
-
-      var newOpensCount: Int = opensCount
-
-      if (chars.head == '(')
-        newOpensCount += 1
-
-      if (chars.head == ')')
-        newOpensCount -= 1
-
-      wereAllBracketsClosed(chars.tail, newOpensCount)
+      if opensCount < 0 || chars.isEmpty then
+        opensCount == 0
+      else
+        if chars.head == '(' then
+          wereAllBracketsClosed(chars.tail, opensCount + 1)
+        else if chars.head == ')' then
+          wereAllBracketsClosed(chars.tail, opensCount - 1)
+        else
+          wereAllBracketsClosed(chars.tail, opensCount)
 
     wereAllBracketsClosed(chars, 0)
-  }
 
   /**
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int =
-    if (coins.isEmpty || money < 0) return 0
-    if (money == 0) return  1
-
-    countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    if coins.isEmpty || money < 0 then
+      0
+    else
+      if money == 0 then
+        1
+      else
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
 
 @main def hello() = println("Hello, World")
